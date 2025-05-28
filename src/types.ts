89f4349ae8b5dde5ec66f83351fa111d7d59e9ab
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-05-28 11:44:09 +0000

    Added icon type to Interface

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,10 @@
-export interface Extension {
+export interface Tool {
   name: string;
   description: string;
+  version: string;
+  numberOfDownloads: number;
+  lastUpdated: string;
+  homepage: string;
   active: boolean;
+  icon: string;
 }
